c3d8cbf3fdf6c06021c12c4d280bb2e14540d498
Greg Delgado <grd888@gmail.com>
2024-08-26 08:30:52 +0800

    Initial Commit

---
+++ BuildSettings/BuildSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildSettingsApp.swift
+//  BuildSettings
+//
+//  Created by Greg Delgado on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct BuildSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
